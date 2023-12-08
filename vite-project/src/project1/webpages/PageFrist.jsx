7cae814ca866cd5e753ee4816c06c927c6f7cc0e
Manohar9010 <gattumanoharg@gmail.com>
2023-12-08 09:51:36 +0530

    add data

--- vite-project/src/project1/webpages/PageFrist.jsx
+++ vite-project/src/project1/webpages/PageFrist.jsx
@@ -3,6 +3,26 @@ import './PageFrist.css'
 
 export default function PageFrist() {
   return (
-    <div></div>
+    <div>
+<div className="mainpage">
+    <div className="main_left">
+        <h1>Hy I Am</h1>
+        <h1>Andrew Thomas</h1>
+        <p>Frontend Devloper with high level of experience In web designing and development,<br/> producting the Quality work </p>
+          <div>
+        <button className='button h_button'>Hire me</button>
+          <div className="main_socialapp">
+            <ul>
+                <li><img src="" alt="" /></li>
+                <li></li>
+                <li></li>
+            </ul>
+          </div>
+          </div>
+    </div>
+    <div className="main_right"></div>
+</div>
+
+    </div>
   )
 }
