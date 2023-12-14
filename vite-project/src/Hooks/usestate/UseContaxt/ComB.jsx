ff9a6009b9093111e8ba8f4be8cd771ddbaab2ad
Manohar9010 <gattumanoharg@gmail.com>
2023-12-14 23:45:35 +0530

    add data

--- vite-project/src/Hooks/usestate/UseContaxt/ComB.jsx
+++ vite-project/src/Hooks/usestate/UseContaxt/ComB.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ComB2 from './ComB2'
-
+import{CityChage} from './UseContaxtCom'
 export default function ComB() {
+let {city}=useContext(CityChage)
   return (
     <div>
+      <h1>{city}</h1>
         <ComB2/>
     </div>
   )
--- vite-project/src/Hooks/usestate/UseContaxt/UseContaxtCom.jsx
+++ vite-project/src/Hooks/usestate/UseContaxt/UseContaxtCom.jsx
@@ -1,12 +1,15 @@
 import React, { createContext, useState } from 'react'
 import ComA from './ComA'
+import ComB from './ComB'
 
 export const ValueChange=createContext()
+export const CityChage=createContext() 
 export default function UseContaxtCom() {
 const[names,SetNames]=useState({
   usename:"shivam",
   Age:"25",
 })
+const [city,setCity]=useState("surat")
   return (
     <div>
     
@@ -15,6 +18,10 @@ const[names,SetNames]=useState({
       <ValueChange.Provider value={names}>
 
         <ComA/>
+        <CityChage.Provider value={{city: city}}>
+          <ComB/>
+        </CityChage.Provider>
+
       </ValueChange.Provider>
         
       
